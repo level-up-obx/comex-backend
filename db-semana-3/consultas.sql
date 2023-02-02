ea585cf73ceba531d1f0712cecf4855e0d3e2872
Joaoorlandojr <joao.ferreira@onebox.one>
2023-02-02 17:56:43 -0300

    relatorio analítico dos pedidos

--- db-semana-3/consultas.sql
+++ db-semana-3/consultas.sql
@@ -10,9 +10,17 @@ INNER JOIN item_pedido ON pedido.id = item_pedido.pedido_id
 INNER JOIN produto ON item_pedido.produto_id = produto.id
 INNER JOIN categoria ON produto.categoria_id = categoria.id;
 
-#Tarefas 05 - Relatório de vendas por categoria
+#Tarefa 05 - Relatório de vendas por categoria
 SELECT categoria.nome AS nome_categoria, SUM(item_pedido.preco_unitario * item_pedido.quantidade) AS total_vendido
 FROM categoria
 JOIN produto ON categoria.id = produto.categoria_id
 JOIN item_pedido ON produto.id = item_pedido.produto_id
 GROUP BY categoria.nome;
+
+#Tarefa 06 - Relatório analítico de pedidos
+SELECT COUNT(DISTINCT pedido.id) AS pedidos_realizados,
+SUM(item_pedido.quantidade) AS produtos_vendidos,
+MIN(item_pedido.preco_unitario * item_pedido.quantidade) AS pedido_mais_barato,
+MAX(item_pedido.preco_unitario * item_pedido.quantidade) AS pedido_mais_caro,
+SUM(item_pedido.preco_unitario * item_pedido.quantidade) AS montante_vendido
+FROM pedido LEFT JOIN item_pedido ON pedido.id = item_pedido.pedido_id;
