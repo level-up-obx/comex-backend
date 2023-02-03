f84cdd94fa5960495f9690e1c136afd15904454f
Fernanda Silva <fernanda.silva@onebox.one>
2023-02-03 11:36:05 -0300

    Adicionando Relatorio completo de pedidos

--- db/consultas.sql
+++ db/consultas.sql
@@ -5,3 +5,19 @@ SELECT categoria.id AS 'CATEGORIA ID', categoria.nome AS 'CATEGORIA NOME',
        produtos.preco AS 'PREÇO PRODUTO'
 FROM categoria
 INNER JOIN produtos ON categoria.id = produtos.categoria_id;
+
+
+use comex;
+SELECT clientes.nome AS 'CLIENTES',
+ categoria.nome AS 'CATEGORIA',
+ produtos.nome AS 'PRODUTOS', 
+ itens_pedidos.preco_unitario AS 'UNITARIO', itens_pedidos.quantidade AS 'QUANTIDADE', 
+ pedidos.data AS 'DATA'
+FROM clientes
+INNER JOIN pedidos ON clientes.id = pedidos.clientes_id
+INNER JOIN itens_pedidos ON pedidos.id = itens_pedidos.pedidos_id
+INNER JOIN produtos ON itens_pedidos.produtos_id = produtos.id
+INNER JOIN categoria ON produtos.categoria_id = categoria.id
+ORDER BY clientes.nome, categoria.nome ASC;
+
+ 
