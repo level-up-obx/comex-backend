890b0a091bae391e7fafa1d9af11f91bbd1f5c64
Mari <marilira8@gmail.com>
2023-02-03 11:50:31 -0300

    Relatório de vendas por categoria

--- db/consultas.sql
+++ db/consultas.sql
@@ -5,7 +5,6 @@ p.preco AS 'PREÇO PRODUTO'
 FROM categoria c INNER JOIN produtos p ON c.id = p.categoria_id;
 
 //Criando consulta que trás um relatório completo de pedidos
-
 SELECT clientes.nome AS 'NOME DO CLIENTE', 
 categoria.nome AS 'CATEGORIA', 
 produtos.nome AS 'NOME DO PRODUTO',
@@ -18,3 +17,11 @@ INNER join itens_pedidos on itens_pedidos.pedido_id = pedidos.id
 INNER join produtos on produtos.id = itens_pedidos.produto_id
 INNER join categoria on produtos.categoria_id = clientes.id
 order by clientes.nome, categoria.nome ASC;
+
+//Relatório de vendas por categoria
+
+SELECT categoria.nome AS 'CATEGORIA', SUM(itens_pedidos.preco_unitario* itens_pedidos.quantidade) as 'TOTAL DE VENDAS'
+from categoria
+inner join produtos on categoria.id = produtos.categoria_id
+inner join itens_pedidos on itens_pedidos.produto_id = produtos.id
+group by categoria.nome;
