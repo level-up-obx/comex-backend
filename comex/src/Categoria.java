b1e0e99628f5eaa43d0ceae5c5568ff3265694e2
Gustavo Nogueira <gustavo.nogueira@onebox.one>
2023-02-08 13:15:07 -0300

    Criando a classe Categoria

---
+++ comex/src/Categoria.java
@@ -0,0 +1,29 @@
+public class Categoria {
+    private Long id;
+    private String nome;
+    private String status = "ATIVA";
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
