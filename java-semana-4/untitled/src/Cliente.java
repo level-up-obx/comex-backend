3232df3aef1f1bec7b0a7052c4ff69abedbbad87
João Orlando <joao.ferreira@onebox.one>
2023-02-09 16:31:45 -0300

    Criando a classe cliente e os métodos de endereço e nome completo.

---
+++ java-semana-4/untitled/src/Cliente.java
@@ -0,0 +1,110 @@
+public class Cliente {
+
+    Long id;
+    String primeiroNome;
+    String sobrenome;
+    String cpf;
+    String telefone;
+    String rua;
+    String numero;
+    String complemento;
+    String bairro;
+    String cidade;
+    String estado;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String nomeCompleto(){
+       return primeiroNome + " " + sobrenome + ".";
+    }
+
+    public String enderecoCompleto(){
+        return rua + " " + numero + " " + bairro + " " + complemento + " " + cidade + " " + estado + ".";
+    }
+}
