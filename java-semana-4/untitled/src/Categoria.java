feb8e8b3fe1141cb19c387dcb052ea08bfe9d3d8
João Orlando <joao.ferreira@onebox.one>
2023-02-09 14:08:19 -0300

    Criando a classe categoria

---
+++ java-semana-4/untitled/src/Categoria.java
@@ -0,0 +1,30 @@
+public class Categoria {
+
+    Long id;
+    String nome;
+    String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
