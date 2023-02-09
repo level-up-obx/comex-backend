1ed08bf2cfeb021f8b79c4d8206f8852586403e9
João Orlando <joao.ferreira@onebox.one>
2023-02-09 15:29:15 -0300

    Criando a classe produto em conjunto com os métodos de calcular valor total em estoque e de calcular imposto

---
+++ java-semana-4/untitled/src/Produto.java
@@ -0,0 +1,71 @@
+import java.math.BigDecimal;
+public class Produto {
+
+    Long id;
+
+    String nome;
+
+    String descricao;
+
+    BigDecimal precoUnitario;
+
+    int quantidadeEmEstoque;
+
+    Categoria categoria;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(BigDecimal precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public int getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+
+    public void setQuantidadeEmEstoque(int quantidadeEmEstoque) {
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public static BigDecimal calculaValorEstoque(BigDecimal precoUnitario, int quantidadeEmEstoque){
+        return precoUnitario.multiply(BigDecimal.valueOf(quantidadeEmEstoque));
+    }
+
+    public static BigDecimal calculaImposto(BigDecimal precoUnitario){
+        return precoUnitario.multiply(BigDecimal.valueOf(0.4));
+    }
+}
