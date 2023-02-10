f5e0005d58b74f3ff23b376a7e3da7ebe849e33c
Tiago Seixas <tiago.silva@onebox.one>
2023-02-10 11:36:38 -0300

    Criação de classe categoria com get e set

---
+++ JavaProject/src/Categoria.java
@@ -0,0 +1,30 @@
+public class Categoria {
+
+    private double id;
+    private String nome;
+    private boolean status;
+
+    public double getId() {
+        return id;
+    }
+
+    public void setId(double id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
