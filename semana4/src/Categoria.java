90c440aa81b5410522d683d1dd51aee466589e7f
Mari <marilira8@gmail.com>
2023-02-11 16:45:56 -0300

    Criando classe categoria

---
+++ semana4/src/Categoria.java
@@ -0,0 +1,39 @@
+
+public class Categoria {
+	   
+	    private int id;
+	    private String nome;
+	    private Boolean status;
+	    
+	    public Categoria(int id, String nome, Boolean status) 
+	    {
+	        this.id = id;
+	        this.nome = nome;
+	        this.status = status;
+        }
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public Boolean getStatus() {
+			return status;
+		}
+
+		public void setStatus(Boolean status) {
+			this.status = status;
+		}
+
+}
