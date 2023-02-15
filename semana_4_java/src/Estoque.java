fd429afe3838cfae1faa310af7062ed6e5490204
Fernanda Silva <fernanda.silva@onebox.one>
2023-02-15 17:02:39 -0300

    Criando classe Estoque

---
+++ semana_4_java/src/Estoque.java
@@ -0,0 +1,43 @@
+import java.math.BigDecimal;
+
+public class Estoque {
+    private int capacidade = 1000;
+    private int ocupacao;
+    private BigDecimal montante = BigDecimal.ZERO;
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public int getOcupacao() {
+        return ocupacao;
+    }
+
+    public BigDecimal getMontante() {
+        return montante;
+    }
+
+    public void registraEntrada(Produto produto) {
+        if (produto.getQuantidadeEstoque() > 0) {
+            int quantidade = produto.getQuantidadeEstoque();
+            if (ocupacao + quantidade > capacidade) {
+                System.out.println("Estoque cheio");
+                return;
+            }
+            ocupacao += quantidade;
+            montante = montante.add(produto.calculaValorTotalEstoque());
+        }
+    }
+
+    public void registraSaida(Produto produto) {
+        if (produto.getQuantidadeEstoque() > 0) {
+            int quantidade = produto.getQuantidadeEstoque();
+            if (ocupacao - quantidade < 0) {
+                System.out.println("Estoque insuficiente");
+                return;
+            }
+            ocupacao -= quantidade;
+            montante = montante.subtract(produto.calculaValorTotalEstoque());
+        }
+    }
+}
