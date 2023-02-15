93bea8ddc7c8641c902d8736edf542d056d79be8
João Orlando <joao.ferreira@onebox.one>
2023-02-15 17:14:07 -0300

    Criação do teste de estoque

---
+++ java-semana-4/comex-backend-semana-quatro/src/TestaEntradaDeProdutoNoEstoque.java
@@ -0,0 +1,26 @@
+import java.math.BigDecimal;
+
+public class TestaEntradaDeProdutoNoEstoque {
+
+    public static void main(String[] args) {
+
+        Categoria categoria1 = new Categoria("Saúde");
+        Categoria categoria2 = new Categoria("Livros");
+        Categoria categoria3 = new Categoria("Informática");
+        ProdutoIsento produtoIsento1 = new ProdutoIsento("Vacina", new BigDecimal("250.00"), 500, categoria1 );
+        ProdutoIsento produtoIsento2 = new ProdutoIsento("Use a Cabeça: Java", new BigDecimal("112.90"), 5, categoria2);
+        ProdutoIsento produto3 = new ProdutoIsento("Macbook Pro", new BigDecimal("20000.00"), 15, categoria3);
+
+        Estoque estoque = new Estoque();
+
+        estoque.registrarEntrada(produtoIsento1);
+        estoque.registrarEntrada(produtoIsento2);
+        estoque.registrarEntrada(produto3);
+
+        System.out.println("Capacidade do estoque: " + estoque.getCapacidade());
+        System.out.println("Ocupação do estoque: " + estoque.getOcupacao());
+        System.out.println("Montante financeiro do estoque: " + estoque.getMontante());
+        
+    }
+
+}
