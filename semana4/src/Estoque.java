ce820466a4a6e425e7745fe6825f744d785b846b
Mari <marilira8@gmail.com>
2023-02-21 23:46:15 -0300

    Criando a classe estoque

---
+++ semana4/src/Estoque.java
@@ -0,0 +1,47 @@
+import java.math.BigDecimal;
+
+public class Estoque {
+
+	private int capacidade = 1000;
+	private int ocupacao;
+	private BigDecimal montante = BigDecimal.ZERO;
+
+	
+	public int getCapacidade() {
+		return capacidade;
+	}
+	public int getOcupacao() {
+		return ocupacao;
+	}
+	public BigDecimal getMontante() {
+		return montante;
+	}
+	
+	public void registraEntrada(Produto produto)
+	{
+		if (produto.getQuantidadeEstoque() > 0) {
+            int quantidade = produto.getQuantidadeEstoque();
+            if (ocupacao + quantidade > capacidade) {
+                System.out.println("Estoque cheio");
+                return;
+            }
+            ocupacao = ocupacao + quantidade;
+            montante = montante.add(produto.ValorTotalEstoque(montante, quantidade));
+        }
+		
+	}
+	
+	public void registraSaida(Produto produto)
+	{
+		if (produto.getQuantidadeEstoque() > 0) {
+            int quantidade = produto.getQuantidadeEstoque();
+            if (ocupacao - quantidade < 0) {
+                System.out.println("Estoque insuficiente");
+                return;
+            }
+            ocupacao = ocupacao + quantidade;
+            montante = montante.subtract(produto.ValorTotalEstoque(montante, quantidade));
+        }
+    }
+	
+}
