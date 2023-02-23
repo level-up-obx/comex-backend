cdad42dee3360326c386a82b14d1492a3e65e9f7
Fernanda Silva <fernanda.silva@onebox.one>
2023-02-23 12:55:54 -0300

    Criando Teste de Validação de Nome para Categoria

---
+++ semana_4_java/src/TestaCategoriaValidacaoNome.java
@@ -0,0 +1,20 @@
+public class TestaCategoriaValidacaoNome {
+    public static void main(String[] args) {
+        Categoria categoria1 = new Categoria(1, "INFORMATICA", Boolean.TRUE);
+
+        System.out.println(categoria1.getNome());
+
+        try {
+            Categoria categoria2 = new Categoria(2, "", Boolean.TRUE);
+        } catch (CategoriaInvalidaException msg) {
+            System.out.println("MSG: " + msg.getMessage());
+        }
+        try {
+            Categoria categoria3 = new Categoria(3, null, Boolean.TRUE);
+        } catch (CategoriaInvalidaException msg) {
+            System.out.println("MSG: " + msg.getMessage());
+        }
+
+
+    }
+}
