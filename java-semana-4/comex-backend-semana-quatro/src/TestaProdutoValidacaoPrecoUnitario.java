88fd1bdcef2f2d4f5180c09f99b738268aec4e5f
João Orlando <joao.ferreira@onebox.one>
2023-02-27 14:21:40 -0300

    criando testes para produto

---
+++ java-semana-4/comex-backend-semana-quatro/src/TestaProdutoValidacaoPrecoUnitario.java
@@ -0,0 +1,13 @@
+import java.math.BigDecimal;
+
+public class TestaProdutoValidacaoPrecoUnitario {
+    public static void main(String[] args) {
+        Categoria categoria4 = new Categoria("Saúde");
+        Categoria categoria5 = new Categoria("Informática");
+        Produto produto1 = new Produto("Vacina", new BigDecimal("250.00"), 1000, categoria4);
+        Produto produto2 = new Produto("Macbook Pro", new BigDecimal("00.00"), 11, categoria5);
+
+        System.out.println(produto1 + "\n" + " e categoria: " + categoria4.getNome() + "." + "\n");
+        System.out.println(produto2 + "\n" + " e categoria: " + categoria5.getNome() + "." + "\n");
+    }
+}
