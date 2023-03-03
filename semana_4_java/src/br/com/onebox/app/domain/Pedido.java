5b6b7c3ced324d517f26d29d1489fbf18b4d15f2
Fernanda Silva <fernanda.silva@onebox.one>
2023-03-03 13:55:58 -0300

    Criando classe Pedido

---
+++ semana_4_java/src/br/com/onebox/app/domain/Pedido.java
@@ -0,0 +1,70 @@
+package br.com.onebox.app.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Pedido {
+    private Categoria categoria;
+    private Produto produto;
+    private Cliente cliente;
+    private BigDecimal preco;
+    private int quantidade;
+    private LocalDate data;
+
+    public Pedido(Categoria categoria, Produto produto, Cliente cliente, BigDecimal preco, int quantidade, LocalDate data) {
+        this.categoria = categoria;
+        this.produto = produto;
+        this.cliente = cliente;
+        this.preco = preco;
+        this.quantidade = quantidade;
+        this.data = data;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+}
