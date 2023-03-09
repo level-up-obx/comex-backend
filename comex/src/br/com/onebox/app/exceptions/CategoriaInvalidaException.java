703b05babce50a4502c246e2787c8f20ed4b66b3
Gustavo Nogueira <gustavo.nogueira@onebox.one>
2023-03-09 14:53:30 -0300

    Criando a classe CategoriaService

---
+++ comex/src/br/com/onebox/app/exceptions/CategoriaInvalidaException.java
@@ -0,0 +1,7 @@
+package br.com.onebox.app.exceptions;
+
+public class CategoriaInvalidaException extends Exception{
+    public CategoriaInvalidaException(String mensagem){
+        super(mensagem);
+    }
+}
---
+++ comex/src/br/com/onebox/app/service/CategoriaService.java
@@ -0,0 +1,27 @@
+package br.com.onebox.app.service;
+
+import br.com.onebox.app.domain.Categoria;
+import br.com.onebox.app.exceptions.CategoriaInvalidaException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoriaService {
+    List<Categoria> categorias = new ArrayList<Categoria>();
+
+    public void cadastrar(Categoria novaCategoria) throws CategoriaInvalidaException {
+        if(novaCategoria.getNome().equals(null) || novaCategoria.getId().equals(null) || novaCategoria.getStatus().equals(null)){
+            throw new CategoriaInvalidaException("Categoria Inválida, valores nulos encontrados!");
+        }
+        categorias.add(novaCategoria);
+        System.out.println("Categoria cadastrada com sucesso");
+    }
+    public Categoria get(Long id) throws CategoriaInvalidaException {
+        if(!id.equals(null) || id < 0){
+            throw new CategoriaInvalidaException("Categoria invalida, id nulo ou menor que zero");
+        }
+        return categorias.stream()
+                .filter(c -> c.getId().equals(id))
+                .findAny().get();
+    }
+}
