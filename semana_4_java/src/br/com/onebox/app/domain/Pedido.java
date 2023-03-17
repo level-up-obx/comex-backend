4e52e8140ff3e3defc0c3938ba910da104bd8dc2
Fernanda Silva <fernanda.silva@onebox.one>
2023-03-17 16:24:52 -0300

    Mapeando classe Pedido (+HotFix mapeam. de Cliente)

--- semana_4_java/src/br/com/onebox/app/domain/Pedido.java
+++ semana_4_java/src/br/com/onebox/app/domain/Pedido.java
@@ -1,21 +1,56 @@
 package br.com.onebox.app.domain;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Comparator;
 
-public class Pedido  {
-    private Cliente cliente;
-    private BigDecimal preco;
-    private int quantidade;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "data_pedidos", nullable = false)
     private LocalDate data;
 
-    public Pedido (Cliente cliente, BigDecimal preco, int quantidade, LocalDate data) {
+    @ManyToOne
+    @JoinColumn(name = "clientes_id", nullable = false)
+    private Cliente cliente;
+
+
+    @Column(name = "desconto_pedidos", nullable = false, precision = 10, scale = 2)
+    private BigDecimal desconto;
+
+
+    @Column(name = "preco_pedidos", nullable = false, precision = 10, scale = 2)
+    private BigDecimal preco;
+
+    @Column(name = "quantidade_pedidos", nullable = false, length = 100)
+    private int quantidade;
+
+
+    public Pedido(Cliente cliente, BigDecimal preco, int quantidade, LocalDate data) {
         this.cliente = cliente;
         this.preco = preco;
         this.quantidade = quantidade;
         this.data = data;
     }
+
+    public Pedido() {
+
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
     public Cliente getCliente() {
         return cliente;
     }
@@ -52,9 +87,11 @@ public class Pedido  {
     public boolean isMaisBaratoQue(Pedido outroPedido) {
         return preco.compareTo(outroPedido.getPreco()) < 0;
     }
+
     public boolean isMaisCaroQue(Pedido outroPedido) {
         return preco.compareTo(outroPedido.getPreco()) > 0;
     }
+
     public BigDecimal getValorTotal() {
         return preco.multiply(BigDecimal.valueOf(quantidade));
     }
@@ -62,10 +99,10 @@ public class Pedido  {
     @Override
     public String toString() {
         return "Pedido" +
-                "\n"+" Cliente:" + cliente.getPrimeiroNome() +
-                "\n"+" Preco:" + preco +
-                "\n"+" Valor Total:" + getValorTotal() +
-                "\n"+" Data:" + data + "\n";
+                "\n" + " Cliente:" + cliente.getPrimeiroNome() +
+                "\n" + " Preco:" + preco +
+                "\n" + " Valor Total:" + getValorTotal() +
+                "\n" + " Data:" + data + "\n";
     }
 
 }
