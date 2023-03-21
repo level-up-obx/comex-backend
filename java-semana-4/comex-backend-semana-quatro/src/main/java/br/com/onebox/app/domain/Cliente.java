85a3f08e6a852ba0f28f7106a66d6e676ac72efb
João Orlando <joao.ferreira@onebox.one>
2023-03-21 13:40:46 -0300

    Criando a classe endereço e utilizando na classe cliente

--- java-semana-4/comex-backend-semana-quatro/src/main/java/br/com/onebox/app/domain/Cliente.java
+++ java-semana-4/comex-backend-semana-quatro/src/main/java/br/com/onebox/app/domain/Cliente.java
@@ -16,18 +16,8 @@ public class Cliente {
     private String cpf;
     @Column(nullable = false, length = 15)
     private String telefone;
-    @Column(nullable = false, length = 100)
-    private String rua;
-    @Column(nullable = false, length = 10)
-    private String numero;
-    @Column(length = 100)
-    private String complemento;
-    @Column(nullable = false, length = 100)
-    private String bairro;
-    @Column(nullable = false, length = 50)
-    private String cidade;
-    @Column(nullable = false, length = 2)
-    private String estado;
+    @Embedded
+    private Endereco endereco;
 
     public Cliente(String primeiroNome, String sobrenome) {
         this.primeiroNome = primeiroNome;
@@ -73,73 +63,18 @@ public class Cliente {
         this.telefone = telefone;
     }
 
-    public String getRua() {
-        return rua;
-    }
 
-    public void setRua(String rua) {
-        this.rua = rua;
-    }
 
-    public String getNumero() {
-        return numero;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-
-    public String getComplemento() {
-        return complemento;
-    }
-
-    public void setComplemento(String complemento) {
-        this.complemento = complemento;
-    }
-
-    public String getBairro() {
-        return bairro;
-    }
-
-    public void setBairro(String bairro) {
-        this.bairro = bairro;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public Cliente(String primeiroNome, String sobrenome, String cpf, String telefone, String rua, String numero, String complemento, String bairro, String cidade, String estado) {
+    public Cliente(String primeiroNome, String sobrenome, String cpf, String telefone) {
         this.primeiroNome = primeiroNome;
         this.sobrenome = sobrenome;
         this.cpf = cpf;
         this.telefone = telefone;
-        this.rua = rua;
-        this.numero = numero;
-        this.complemento = complemento;
-        this.bairro = bairro;
-        this.cidade = cidade;
-        this.estado = estado;
+
     }
 
     public String nomeCompleto(){
        return primeiroNome + " " + sobrenome;
-    }
-
-    public String enderecoCompleto(){
-        return rua + " " + numero + " " + bairro + " " + complemento + " " + cidade + " " + estado;
     }
 
 }
--- java-semana-4/comex-backend-semana-quatro/src/main/java/br/com/onebox/app/tests/TestaCliente.java
+++ java-semana-4/comex-backend-semana-quatro/src/main/java/br/com/onebox/app/tests/TestaCliente.java
@@ -6,14 +6,11 @@ public class TestaCliente {
 
     public static void main(String[] args) {
         Cliente cliente1 = new Cliente("João", "Orlando", "333.333.333-33",
-                "+5511958023432", "Rua Getúlio Vargas,", "190,",
-                "Sem complemento,", "Tancredo Neves,", "São Paulo,", "São Paulo." + "\n");
+                "+5511958023432" + "\n");
         Cliente cliente2 = new Cliente("Gustavo", "Ferreira", "222.222.222-22",
-                "+5511957022431", "Rua Fernão Dias,", "180,",
-                "Apartamento 23, bloco 8,", "Ayrton Senna,", "São Paulo,", "São Paulo." + "\n");
+                "+5511957022431" + "\n");
         Cliente cliente3 = new Cliente("Fernando", "Junior", "444.444.444-43",
-                "+5511942013432", "Rua 7 de Agosto,", "170,",
-                "Sem complemento,", "Liberdade,", "São Paulo,", "São Paulo." + "\n");
+                "+5511942013432" + "\n");
 
         System.out.println(cliente1);
         System.out.println(cliente2);
