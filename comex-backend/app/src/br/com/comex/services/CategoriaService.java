685dfc2cb1ba099a328088ffaa580e438fb71c45
dnedaniellaurindo <daniel.laurindo@onebox.one>
2023-03-21 00:11:22 -0300

    criar CategoriaService

---
+++ comex-backend/app/src/br/com/comex/services/CategoriaService.java
@@ -0,0 +1,18 @@
+package br.com.comex.services;
+
+import br.com.comex.entidades.Categoria;
+
+public class CategoriaService {
+	
+	//
+	// Crie um método que receba uma nova categoria e faça a devida validação 
+	// se ela é ou não válida para posteriormente salvarmos no banco de dados.
+	
+	public void cadastrar(Categoria novaCategoria){
+		
+	}
+	
+	public Categoria getCategoria(Long id) {
+		return null;
+	}
+}
