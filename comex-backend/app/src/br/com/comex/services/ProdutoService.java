1088b3e3543b239fa9c628d50f06137825157a4d
dnedaniellaurindo <daniel.laurindo@onebox.one>
2023-03-21 00:12:45 -0300

    criar ProdutoService

---
+++ comex-backend/app/src/br/com/comex/services/ProdutoService.java
@@ -0,0 +1,15 @@
+package br.com.comex.services;
+
+import br.com.comex.entidades.Produto;
+
+public class ProdutoService {
+	
+	public void cadastrar(Produto novoProduto){
+
+	}
+	
+	public Produto getProdutoById(Long id) {
+		return null;
+	}
+}
+
