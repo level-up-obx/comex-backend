9e89eafdbdbb74cf7e12f766e24039bc461e228e
Gustavo Nogueira <gustavo.nogueira@onebox.one>
2023-03-23 14:58:20 -0300

    Criando a interface PedidoRepository

---
+++ comex/src/main/java/br/com/onebox/app/repository/PedidoRepository.java
@@ -0,0 +1,11 @@
+package br.com.onebox.app.repository;
+
+import br.com.onebox.app.domain.Pedido;
+import org.springframework.data.repository.CrudRepository;
+
+import java.time.LocalDate;
+
+public interface PedidoRepository extends CrudRepository<Pedido, Long> {
+
+    public Iterable<Pedido> findByDate(LocalDate data);
+}
