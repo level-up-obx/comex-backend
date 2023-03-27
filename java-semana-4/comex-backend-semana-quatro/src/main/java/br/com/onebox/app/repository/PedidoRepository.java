a8dd8f4c8bfa927f1331f55938a05ce2c01356b9
João Orlando <joao.ferreira@onebox.one>
2023-03-26 23:39:19 -0300

    Refatorando pedido dao para pedido repository

---
+++ java-semana-4/comex-backend-semana-quatro/src/main/java/br/com/onebox/app/repository/PedidoRepository.java
@@ -0,0 +1,17 @@
+package br.com.onebox.app.repository;
+
+import br.com.onebox.app.domain.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.persistence.EntityManager;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    List<Pedido> findAll();
+    @Query("SELECT p FROM Pedido p WHERE p.data = :data")
+    List<Pedido> buscaPorData(@Param("data") LocalDate data);
+
+}
--- java-semana-4/comex-backend-semana-quatro/src/main/java/br/com/onebox/app/repository/ProdutoRepository.java
+++ java-semana-4/comex-backend-semana-quatro/src/main/java/br/com/onebox/app/repository/ProdutoRepository.java
@@ -1,13 +1,14 @@
 package br.com.onebox.app.repository;
 
 import br.com.onebox.app.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
-public interface ProdutoRepository {
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
    List<Produto> findAll();
    @Query("SELECT p FROM Produto p WHERE p.quantidadeEmEstoque = 0")
    List<Produto> listaIndisponiveis(@Param("quantidadeEmEstoque") int quantidadeEmEstoque);
